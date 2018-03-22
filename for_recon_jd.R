55a7f988c371049ca184a0d727e5bfb2744d9113
Jong Dae Kim <arcgura@gmail.com>
2018-03-22 22:47:59 +0900

    minor change

--- for_recon_jd.R
+++ for_recon_jd.R
@@ -13,18 +13,77 @@ library(readAny)
 
 
 
+# 1st renaming
+
 i.path <- paste0("~/Dropbox (My working)/team_folder/raw_image/re_jd4")
 
 
 file.list <- read_exif(i.path, recursive = TRUE) %>%
-        select(SourceFile, FileName, Directory, DateTimeOriginal) %>%
+        select(SourceFile, FileName, Directory, DateTimeOriginal, FileType) %>%
         filter(FileType == "JPEG")
+
+temp <- file.list
 
 file.time <- strptime(file.list$DateTimeOriginal, format = "%Y:%m:%d %H:%M:%S")
 
 
 file.list <- file.list %>% 
-        mutate(NewName = paste0("-", format(file.time, format = "%y%m%d"), "-re_jd4-", FileName),
-               NewDirectory = paste0("//Burndoc3/김종대 의국4/_재건외래 JD/temp", NewName))
+        mutate(NewName = paste0("-", format(file.time, format = "%y%m%d"), "-re_jd4-", FileName))
+               
 
-file.rename(file.list$SourceFile, file.list$NewDirectory)
+file.rename(file.list$SourceFile, paste0(file.list$Directory, "/", file.list$NewName))
+
+
+
+
+# copy to imageDB according to unit number
+
+rena.list.1 <- data.frame(FileName = list.files("~/Dropbox (My working)/team_folder/raw_image/re_jd4"))
+
+rena.list.2 <- rena.list.1 %>% mutate(xxx = FileName) %>%
+                        separate(xxx, c("unit", "date", "place", "raw_file", "etc"), "-", extra = "merge", fill = "right") %>%
+                        filter(!is.na(date)) %>% filter(as.numeric(unit) < 500000)
+
+
+
+list.leng <- nrow(rena.list.2)
+
+
+for (i in 1:list.leng) {
+                new.dir <- paste0("~/Dropbox (My working)/team_folder/Image_DB/", rena.list.2$unit[i])
+                dir.create(new.dir, showWarnings = FALSE)
+                file.copy(paste0("~/Dropbox (My working)/team_folder/raw_image/re_jd4/", rena.list.2$FileName[i]), 
+                          paste0(new.dir, "/", rena.list.2$FileName[i]))
+        
+        
+}
+
+
+# move to JD_DB
+
+
+recon <- data.frame(Directory = list.files("//Burndoc3/김종대 의국4/_재건외래 JD/_0. Pts"))
+
+for (i in 1:list.leng) {
+        
+        
+        a <- rena.list.2$unit[i]
+        
+        if (sum(grepl(a, recon$Directory)) == 1 ) {
+             b <- recon[grepl(a, recon$Directory), "Directory"]
+             new.dir <- paste0("//Burndoc3/김종대 의국4/_재건외래 JD/_0. Pts/", b)
+             
+             file.rename(paste0("~/Dropbox (My working)/team_folder/raw_image/re_jd4/", rena.list.2$FileName[i]), 
+                         paste0(new.dir, "/", rena.list.2$FileName[i]))
+        }
+        if (sum(grepl(a, recon$Directory)) == 0 ){
+             new.dir <- paste0("//Burndoc3/김종대 의국4/_재건외래 JD/_0. Pts/", rena.list.2$unit[i])
+             dir.create(new.dir, showWarnings = FALSE)
+             file.rename(paste0("~/Dropbox (My working)/team_folder/raw_image/re_jd4/", rena.list.2$FileName[i]), 
+                         paste0(new.dir, "/", rena.list.2$FileName[i]))
+        }
+        
+}
+
+b
+
--- original-initial_rename.R
+++ original-initial_rename.R
@@ -1,5 +1,6 @@
 # version history
 # v0319  #1  file.list에서 ColorTemperature 항목을 제거함
+# 180322 : file directory를 변경함
 
 
 
@@ -12,7 +13,7 @@ library(exifr)
 rn.1st <- function(place){
 
 
-                i.path <- paste0("C:/Users/JD/Dropbox/team_folder/raw_image/", place)
+                i.path <- paste0("~/Dropbox (My working)/team_folder/raw_image/", place)
 
                 file.list <- read_exif(i.path, recursive = TRUE) %>%
                         select(SourceFile, FileName, Directory, FileSize, FileType, DateTimeOriginal, Make, Model, 
@@ -24,7 +25,7 @@ rn.1st <- function(place){
 
                 file.list <- file.list %>% 
                         mutate(NewName = paste0("-", format(file.time, format = "%y%m%d"), "-", place, "-", FileName),
-                               NewDirectory = paste0("C:/Users/JD/Dropbox/team_folder/raw_image/temp_for_rename/", NewName))
+                               NewDirectory = paste0("~/Dropbox (My working)//team_folder/raw_image/temp_for_rename/", NewName))
 
                 file.rename(file.list$SourceFile, file.list$NewDirectory)
                 
